c82886c8d9ae27e4716edcf8a14b622e8c3a0439
Ege Karaismailoglu <karaismailoglu.ege@gmail.com>
2024-01-19 17:47:49 +0100

    created env setup script, updated README, added TopoNetX

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # topological-equivariant-networks
 Topological Equivariant Networks (TEN)
+
+## Setup instructions
+```
+git clone git@github.com:NSAPH-Projects/topological-equivariant-networks.git
+chmod +x env_builder.sh
+source env_builder.sh
+```
+This will create and activate a new conda environment called "ten" that contains all the packages necessary to run the code in this repository. 
---
+++ env_builder.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script Name: env_builder.sh
+# Description: 
+#   This script automates the process of setting up a Conda environment
+#   and installing a package from a Git repository. It checks for Conda,
+#   creates or activates the specified environment, clones the repository,
+#   and installs the package in editable mode.
+#
+# Usage: source env_builder.sh
+# Requirements:
+#   - Conda must be installed and initialized in your system.
+#   - Git must be installed and accessible from the command line.
+#   - Internet access is required to clone the repository.
+#
+# Notes:
+#   - Ensure the script has executable permissions: chmod +x setup_conda_env.sh
+
+# Check if Conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda could not be found. Please install Conda and retry."
+    exit 1
+fi
+
+# Activate or create Conda environment
+echo "Creating environment 'ten'"
+conda env create -f environment.yaml
+conda activate ten
+
+# Install TopoNetX
+echo "Installing 'TopoNetX'"
+REPO_URL="https://github.com/pyt-team/TopoNetX"
+git clone "$REPO_URL"
+REPO_DIR=$(basename "$REPO_URL" .git)
+cd "$REPO_DIR"
+git pull
+pip install -e '.[all]'
+
+echo "Installation completed."

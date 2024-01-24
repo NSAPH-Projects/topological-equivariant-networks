fab885bcdb989e8d0921779f58d8fe87c24b23ae
Ege Karaismailoğlu <karaismailoglu.ege@gmail.com>
2024-01-24 09:23:16 +0100

    added fc_nodes to rips_lift for reproducibility

--- simplicial_data/lifts.py
+++ simplicial_data/lifts.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 import gudhi
 
 
@@ -5,12 +7,17 @@ def clique_lift(graph, dim, dis) -> list[list[int]]:
     raise NotImplementedError
 
 
-def rips_lift(graph, dim, dis) -> list[list[int]]:
+def rips_lift(graph, dim, dis, fc_nodes: bool = True) -> list[list[int]]:
     # create simplicial complex
     x_0, pos = graph.x, graph.pos
     points = [pos[i].tolist() for i in range(pos.shape[0])]
     rips_complex = gudhi.RipsComplex(points=points, max_edge_length=dis)
     simplex_tree = rips_complex.create_simplex_tree(max_dimension=dim)
+
+    if fc_nodes:
+        nodes = [i for i in range(x_0.shape[0])]
+        for edge in combinations(nodes, 2):
+            simplex_tree.insert(edge)
 
     # convert simplicial complex to list of lists
     simplexes = []

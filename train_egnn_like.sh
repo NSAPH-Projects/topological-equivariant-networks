564c14faa61283ca90b1084fe1d845475212bf7f
Ege Karaismailoglu <karaismailoglu.ege@gmail.com>
2024-03-19 18:22:00 -0400

    create training script with egnn-like hps

---
+++ train_egnn_like.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to train an EGNN-like model on the QM9 dataset. Run using `source train_egnn_like.sh` 
+
+# The following are missing from the EGNN specification:
+# 1. EGNN uses a different data split
+# 2. There MAY be slight differences between EGNN and EMPSN layers, this needs to be checked
+
+conda activate ten
+python src/main_qm9.py --lifters "atom:0" "supercell:1" \
+                       --target_name "alpha" \
+                       --neighbor_type "any_adjacency" \
+                       --connectivity "self" \
+                       --post_pool_filter "0" \
+                       --epochs 1000 \
+                       --batch_size 96 \
+                       --weight_decay 1e-16 \
+                       --lr 5e-4 \
+                       --num_layers 7 \
+                       --num_hidden 128

12eb404ce4d489b78d7ab97bee0afa2a54850a4b
Ege Karaismailoglu <ekaraismailoglu@holygpu7c26306.rc.fas.harvard.edu>
2024-05-10 17:30:29 -0400

    adapt ranker tests

--- tests/unit/combinatorial_data/test_ranker.py
+++ tests/unit/combinatorial_data/test_ranker.py
@@ -1,36 +1,44 @@
+from argparse import Namespace
+
 import pytest
 
-from combinatorial_data.ranker import get_ranker
+from combinatorial_data.lifter import Lifter
+from qm9.lifts.registry import lifter_registry
 
 
 @pytest.mark.parametrize(
     "lifter_args, cell, memberships, expected",
     [
         # simple 1
-        (["identity:c", "ring:2"], ["a", "b", "c"], [True, False], 2),
+        (["clique:c", "ring:2"], ["a", "b", "c"], [True, False], 2),
         # simple 2
-        (["identity:c", "ring:2"], ["a", "b"], [False, True], 2),
+        (["clique:c", "ring:2"], ["a", "b"], [False, True], 2),
         # simple 3
-        (["identity:c", "ring:2"], ["a", "b"], [True, True], 1),
+        (["clique:c", "ring:2"], ["a", "b"], [True, True], 1),
         # no lifters
-        (["identity:c", "ring:2"], ["a", "b", "c"], [False, False], ValueError),
+        (["clique:c", "ring:2"], ["a", "b", "c"], [False, False], ValueError),
         # length mismatch
-        (["identity:c", "ring:2"], ["a", "b"], [True], ValueError),
+        (["clique:c", "ring:2"], ["a", "b"], [True], ValueError),
         # negative rank
-        (["identity:-1"], ["a", "b"], [True], ValueError),
+        (["clique:-1"], ["a", "b"], [True], ValueError),
         # unspecified rank
-        (["identity"], ["a", "b"], [True], 1),
+        (["clique"], ["a", "b"], [True], 1),
         # unknown/illegal rank
-        (["identity:abc"], ["a", "b"], [True], ValueError),
+        (["clique:abc"], ["a", "b"], [True], ValueError),
     ],
 )
-def test_get_ranker(
+def test_ranker(
     lifter_args: list[str], cell: frozenset[int], memberships: list[bool], expected: int
 ):
+
+    # Create the lifter
+    args = Namespace(lifters=lifter_args, dim=3)
+
+    # Test the ranker
     if expected is ValueError:
         with pytest.raises(ValueError):
-            ranker = get_ranker(lifter_args)
-            ranker(cell, memberships)
+            lifter = Lifter(args, lifter_registry)
+            lifter.ranker(cell, memberships)
     else:
-        ranker = get_ranker(lifter_args)
-        assert ranker(cell, memberships) == expected
+        lifter = Lifter(args, lifter_registry)
+        assert lifter.ranker(cell, memberships) == expected

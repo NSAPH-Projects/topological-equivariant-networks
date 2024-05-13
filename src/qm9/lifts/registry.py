903b56e50e952499f0025babd17bc68a90f38c74
Ege Karaismailoglu <ekaraismailoglu@holygpu7c26306.rc.fas.harvard.edu>
2024-05-13 11:02:10 -0400

    distinguish edge_lift() and bond_lift() in the registry

--- src/qm9/lifts/registry.py
+++ src/qm9/lifts/registry.py
@@ -1,5 +1,5 @@
 from .atom import node_lift
-from .bond import edge_lift
+from .bond import bond_lift, edge_lift
 from .clique import clique_lift
 from .functional_group import functional_group_lift
 from .molecule import supercell_lift
@@ -8,7 +8,7 @@ from .rips_vietoris_complex import rips_lift
 
 lifter_registry = {
     "atom": node_lift,  # atom is an alias for node
-    "bond": edge_lift,  # bond is an alias for edge
+    "bond": bond_lift,
     "clique": clique_lift,
     "edge": edge_lift,
     "functional_group": functional_group_lift,

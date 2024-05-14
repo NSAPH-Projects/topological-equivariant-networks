b846fc79db2c210c66520da7e5be0ba27429f6bb
Ege Karaismailoglu <ekaraismailoglu@holygpu7c26305.rc.fas.harvard.edu>
2024-05-14 15:57:42 -0400

    replace QM9 with QM9_CC

--- src/qm9/utils.py
+++ src/qm9/utils.py
@@ -3,16 +3,15 @@ import json
 import os
 import pickle
 import random
+import sys
 from argparse import Namespace
-from typing import Dict, List, Tuple
 
 import torch
-from rdkit import Chem
-from rdkit.Chem import AllChem, rdMolDescriptors
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torch_geometric.data import Data
 from torch_geometric.datasets import QM9
+from torch_geometric.utils import one_hot, scatter
 from tqdm import tqdm
 
 from combinatorial_data.combinatorial_data_utils import (
@@ -22,6 +21,167 @@ from combinatorial_data.combinatorial_data_utils import (
 )
 from combinatorial_data.lifter import Lifter
 from qm9.lifts.registry import lifter_registry
+
+HAR2EV = 27.211386246
+KCALMOL2EV = 0.04336414
+
+conversion = torch.tensor(
+    [
+        1.0,
+        1.0,
+        HAR2EV,
+        HAR2EV,
+        HAR2EV,
+        1.0,
+        HAR2EV,
+        HAR2EV,
+        HAR2EV,
+        HAR2EV,
+        HAR2EV,
+        1.0,
+        KCALMOL2EV,
+        KCALMOL2EV,
+        KCALMOL2EV,
+        KCALMOL2EV,
+        1.0,
+        1.0,
+        1.0,
+    ]
+)
+
+
+class QM9_CC(QM9):
+    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+        super(QM9_CC, self).__init__(root, transform, pre_transform, pre_filter)
+
+    def process(self) -> None:
+        try:
+            import rdkit
+            from rdkit import Chem, RDLogger
+            from rdkit.Chem.rdchem import BondType as BT
+            from rdkit.Chem.rdchem import HybridizationType
+
+            RDLogger.DisableLog("rdApp.*")
+
+        except ImportError:
+            rdkit = None
+
+        if rdkit is None:
+            print(
+                (
+                    "Using a pre-processed version of the dataset. Please "
+                    "install 'rdkit' to alternatively process the raw data."
+                ),
+                file=sys.stderr,
+            )
+
+            data_list = torch.load(self.raw_paths[0])
+            data_list = [Data(**data_dict) for data_dict in data_list]
+
+            if self.pre_filter is not None:
+                data_list = [d for d in data_list if self.pre_filter(d)]
+
+            if self.pre_transform is not None:
+                data_list = [self.pre_transform(d) for d in data_list]
+
+            self.save(data_list, self.processed_paths[0])
+            return
+
+        types = {"H": 0, "C": 1, "N": 2, "O": 3, "F": 4}
+        bonds = {BT.SINGLE: 0, BT.DOUBLE: 1, BT.TRIPLE: 2, BT.AROMATIC: 3}
+
+        with open(self.raw_paths[1], "r") as f:
+            target = [
+                [float(x) for x in line.split(",")[1:20]] for line in f.read().split("\n")[1:-1]
+            ]
+            y = torch.tensor(target, dtype=torch.float)
+            y = torch.cat([y[:, 3:], y[:, :3]], dim=-1)
+            y = y * conversion.view(1, -1)
+
+        with open(self.raw_paths[2], "r") as f:
+            skip = [int(x.split()[0]) - 1 for x in f.read().split("\n")[9:-2]]
+
+        suppl = Chem.SDMolSupplier(self.raw_paths[0], removeHs=False, sanitize=False)
+
+        data_list = []
+        for i, mol in enumerate(tqdm(suppl)):
+            if i in skip:
+                continue
+            N = mol.GetNumAtoms()
+
+            conf = mol.GetConformer()
+            pos = conf.GetPositions()
+            pos = torch.tensor(pos, dtype=torch.float)
+
+            type_idx = []
+            atomic_number = []
+            aromatic = []
+            sp = []
+            sp2 = []
+            sp3 = []
+            num_hs = []
+            for atom in mol.GetAtoms():
+                type_idx.append(types[atom.GetSymbol()])
+                atomic_number.append(atom.GetAtomicNum())
+                aromatic.append(1 if atom.GetIsAromatic() else 0)
+                hybridization = atom.GetHybridization()
+                sp.append(1 if hybridization == HybridizationType.SP else 0)
+                sp2.append(1 if hybridization == HybridizationType.SP2 else 0)
+                sp3.append(1 if hybridization == HybridizationType.SP3 else 0)
+
+            z = torch.tensor(atomic_number, dtype=torch.long)
+
+            rows, cols, edge_types = [], [], []
+            for bond in mol.GetBonds():
+                start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+                rows += [start, end]
+                cols += [end, start]
+                edge_types += 2 * [bonds[bond.GetBondType()]]
+
+            edge_index = torch.tensor([rows, cols], dtype=torch.long)
+            edge_type = torch.tensor(edge_types, dtype=torch.long)
+            edge_attr = one_hot(edge_type, num_classes=len(bonds))
+
+            perm = (edge_index[0] * N + edge_index[1]).argsort()
+            edge_index = edge_index[:, perm]
+            edge_type = edge_type[perm]
+            edge_attr = edge_attr[perm]
+
+            row, col = edge_index
+            hs = (z == 1).to(torch.float)
+            num_hs = scatter(hs[row], col, dim_size=N, reduce="sum").tolist()
+
+            x1 = one_hot(torch.tensor(type_idx), num_classes=len(types))
+            x2 = (
+                torch.tensor([atomic_number, aromatic, sp, sp2, sp3, num_hs], dtype=torch.float)
+                .t()
+                .contiguous()
+            )
+            x = torch.cat([x1, x2], dim=-1)
+
+            name = mol.GetProp("_Name")
+            smiles = Chem.MolToSmiles(mol, isomericSmiles=True)
+
+            data = Data(
+                x=x,
+                z=z,
+                pos=pos,
+                edge_index=edge_index,
+                smiles=smiles,
+                edge_attr=edge_attr,
+                y=y[i].unsqueeze(0),
+                name=name,
+                idx=i,
+            )
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+
+            data_list.append(data)
+
+        self.save(data_list, self.processed_paths[0])
 
 
 def calc_mean_mad(loader: DataLoader) -> tuple[Tensor, Tensor]:
@@ -92,7 +252,7 @@ def lift_qm9_to_cc(args: Namespace) -> list[dict]:
     the CombinatorialComplexData class. We transform to dictionary format to allow for storage as
     JSON files.
     """
-    qm9 = QM9("./datasets/QM9")
+    qm9 = QM9_CC("./datasets/QM9")
     # Create the transform
     lifter = Lifter(args, lifter_registry)
     transform = CombinatorialComplexTransform(

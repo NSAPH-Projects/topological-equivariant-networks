9d1bd7d33aec2a646695c78f7e5deece30aeed09
audiracmichelle <audiracmichelle@gmail.com>
2024-06-21 18:11:59 -0400

    fixed minor details

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,16 +1,17 @@
 import hydra
 from qm9.utils import process_qm9_dataset
-from utils import set_seed
+from omegaconf import DictConfig
+#from utils import set_seed
 
-@hydra.main(config_path="conf_qm9", config_name="config", version_base=None)
+@hydra.main(config_path="../conf/conf_qm9", config_name="config", version_base=None)
 def main(cfg: DictConfig):
     #set_seed(args.seed)
     process_qm9_dataset(
-        cfg.lifter.lifter_names, 
-        cfg.lifter.neighbor_types, 
+        list(cfg.lifter.lifter_names), 
+        list(cfg.lifter.neighbor_types), 
         cfg.lifter.connectivity, 
-        cfg.lifter.visible_dims,  
-        cfg.lifter.initial_features, 
+        list(cfg.lifter.visible_dims), 
+        list(cfg.lifter.initial_features), 
         cfg.lifter.dim, 
         cfg.lifter.dis,
         cfg.lifter.merge_neighbors,

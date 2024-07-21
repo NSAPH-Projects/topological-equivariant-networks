55d6f62ed963eb734181c89668eda08d07f021bc
audiracmichelle <audiracmichelle@gmail.com>
2024-07-21 16:27:24 -0400

    remove unused files Fixes #59

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# contents of tests/conftest.py
-import sys
-from pathlib import Path
-
-# Assuming the structure is project_root/src and project_root/tests
-repo_root = Path(__file__).parent.parent
-sys.path.insert(0, str(repo_root / "src"))
